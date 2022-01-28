93decadc49cbd7ae946bcf216fb1b20a53ae9c5d
Diego Lago <20dlagopaz01@gmail.com>
2022-01-28 20:29:09 +0100

    Menu pasandose a clase

--- pruebaDialogos/main.py
+++ pruebaDialogos/main.py
@@ -1,14 +1,18 @@
 import pygame, pygame_menu
 from pygame_menu import Theme
 
+from pruebaDialogos.menu import Menu
+
 pygame.init()
 surface = pygame.display.set_mode((600, 400))
+
+menu=Menu()
 
 #Métodos
 def set_difficulty(value, difficulty):
     pass
 
-pygame.display.set_caption('Prueba menu')
+pygame.display.set_caption('SeaBass')
 
 #TEXTO
 white = (255, 255, 255)
@@ -17,6 +21,7 @@ green = (0, 255, 0)
 blue = (0, 0, 128)
 X = 400
 Y = 400
+nombre=""
 clock = pygame.time.Clock()
 font = pygame.font.Font('freesansbold.ttf', 32)
 text = font.render('Fin de la prueba', True, white, blue)
@@ -24,7 +29,6 @@ textRect = text.get_rect()
 textRect.center = (X // 2, Y // 2)
 
 def pantallaInicio():
-
     contadorTiempo = 0
     imagen = pygame.image.load("logo.png")
     #Pantalla en blanco
@@ -41,7 +45,6 @@ def pantallaInicio():
         if(i<255):
             i = i + 0.5
             imagen.set_alpha(i)
-            print(i)
         surface.blit(imagen, (0, 0))
         pygame.display.flip()
         contadorTiempo=contadorTiempo + 1
@@ -52,7 +55,6 @@ def pantallaInicio():
         if(i>0):
             i = i - 1
             imagen.set_alpha(i)
-            print(i)
         surface.fill(black)
         surface.blit(imagen, (0, 0))
         pygame.display.flip()
@@ -70,20 +72,24 @@ def iniciar():
         surface.fill((255,255,255,255))
         surface.blit(text, textRect)
         pygame.display.flip()
-
         is_running = True
         while is_running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     is_running = False
-
         pygame.quit()
 
+def MyTextValue(name):
+    print("Nombre: "+name)
+    nombre=name
+def guardarNombre():
+    print("Nombre final:"+nombre)
+    iniciar()
 def nuevaPartida():
     #Creacion do submenu
     submenu = pygame_menu.Menu('', 300, 400,theme=mytheme)
-    submenu.add.text_input('Nombre:', input_underline='-', maxchar=(11), input_underline_vmargin=6)
-    submenu.add.button('Confirmar', iniciar)
+    submenu.add.text_input('Nombre:', input_underline='-', maxchar=(11), input_underline_vmargin=6, onchange= MyTextValue)
+    submenu.add.button('Confirmar', guardarNombre)
     submenu.add.button('Atras', pygame_menu.events.RESET)
     return submenu
 
@@ -91,7 +97,8 @@ def cargarPartida():
     #Creacion do submenu
     submenu = pygame_menu.Menu('', 300, 400,theme=mytheme)
     #Los datos del selector se obtendrán de la bbdd
-    submenu.add.selector('Nombre :', [('Diego Lago', 1), ('Juan Carlos', 2)], onchange=set_difficulty)
+    listadoJugadores=[('Diego Lago', 1), ('Juan Carlos', 2)]
+    submenu.add.selector('Nombre :', listadoJugadores, onchange=set_difficulty)
     submenu.add.button('Confirmar', iniciar)
     submenu.add.button('Atras', pygame_menu.events.RESET)
     return submenu
